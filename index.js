edbbbafbc5f17d338d298ab6101550bc4536753f
patricio <patoare@hotmail.com>
2024-12-11 14:18:51 +0100

    Redone

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []; 
+    this.length = 0; 
+  }
+  
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length += 1;
+  }
 
-  add(item) {}
+  get(pos) {
+    const elementReturn = this.items[pos]
+    if(elementReturn) {
+       return elementReturn
+    }
+    throw new Error('OutOfBounds')
+  }
 
-  get(pos) {}
-
-  max() {}
-
-  min() {}
+  max() {
+      if (this.length === 0) {
+        throw new Error("EmptySortedList");
+      }
+      return this.items[this.length - 1];
+    }
+  
+    min() {
+      if (this.length === 0) {
+        throw new Error("Empty SortedList");
+      }
+      return this.items[0];
+  }
+  }
 
   sum() {}
 
   avg() {}
-}
+
 
 module.exports = SortedList;
+*/
